63d7c617986c3c1b9ab9b826a1bebdd5d842629d
DeViLINSIDE <32457916+SparshKat@users.noreply.github.com>
2019-10-22 21:09:25 +0530

    DIV2A 490A {Team Olympiad}
    Div 2A. 490A code solution . Question name - Team Olympiad

--- cf.cpp
+++ cf.cpp
@@ -1,39 +1,38 @@
 #include<iostream>
-#include<math.h>
+#include<stack>
 using namespace std;
 
 
-
 int main(){
-    int x1,x2,y1,y2,x3,x4,y3,y4;
-    cin >> x1 >> y1>>x2>>y2;
-    int x_diff = x1-x2;
-    int y_diff = y1-y2;
-    if(x_diff!=0 && y_diff!=0){
-        x3= x1;
-        x4 = x2;
-        y3 = y2;
-        y4 = y1;
-    } else if(x_diff!=0){
-        x3= x1;
-        x4 = x2;
-        y3 = y1 + abs(x_diff);
-        y4 = y2 + abs(x_diff);
-    } else if(y_diff !=0){
-        x3 = x1 + abs(y_diff);
-        x4 = x2 + abs(y_diff);
-        y3 = y1;
-        y4 = y2;
-    } else {
-        x3 = x1 + abs(y_diff);
-        x4 = x2 + abs(y_diff);
-        y3 = y1;
-        y4 = y2;
-    } 
-    if(x_diff!=0 && y_diff!=0 && abs(x_diff) != abs(y_diff)){
-        cout <<"-1"<<endl;
-    } else {
-        cout << x3 << " " << y3 << " " << x4 << " " << y4 << endl;
+    int flag= 0;
+    stack<int> s1,s2,s3;
+    int n ;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        int el = 0;
+        cin >> el;
+        if(el==1){
+            s1.push(i);
+            continue;
+        } else if(el==2){
+            s2.push(i);
+        } else if(el==3){
+            s3.push(i);
+        }
     }
+    int el1,el2,el3 ;
+    int temp = min(s1.size() , min(s2.size() , s3.size()));
+    if(temp!=0) cout << temp << endl;
+    
+    while(!s1.empty() && !s2.empty() && !s3.empty()){
+        flag = 1;
+        el1 = s1.top();
+        el2 = s2.top();
+        el3 = s3.top();
+        cout << el1 << " " << el2 << " "<<el3 <<endl;
+        s1.pop(); s2.pop(); s3.pop();
+    }
+    if(flag==0) cout << "0"<<endl;
     return 0;
+    
 }
